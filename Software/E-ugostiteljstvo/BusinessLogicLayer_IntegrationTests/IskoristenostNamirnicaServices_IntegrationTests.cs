30f71c1de35e2bad4d462a34d0b6d84f9f8c4408
Lovro Pejakovic <lpejakovi20@foi.hr>
2023-06-06 20:44:45 +0200

    Implementiran dio integracijskih testova za klasu IskoristenostNamirnicaServices.

---
+++ Software/E-ugostiteljstvo/BusinessLogicLayer_IntegrationTests/IskoristenostNamirnicaServices_IntegrationTests.cs
@@ -0,0 +1,78 @@
+﻿using BusinessLogicLayer.Services;
+using DataAccessLayer.Repositories;
+using EntitiesLayer;
+using EntitiesLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using Xunit;
+
+namespace BusinessLogicLayer_IntegrationTests
+{
+    public class IskoristenostNamirnicaServices_IntegrationTests
+    {
+        [Fact]
+        public void AddIskoristenostNamirnice_GivenIskoristenostNamirniceIsNotNull_IskoristenostNamirniceIsAdded()
+        {
+            //Arrange
+            using (var transaction = new TransactionScope())
+            {
+                var iskoristenost = new iskoristenost_namirnice()
+                {
+                    iskoristeno = 99,
+                    namirnica_u_katalogu_id = 552182865,
+                    datum = DateTime.Now
+                };
+                IskoristenostNamirnicaServices service = new IskoristenostNamirnicaServices(new IskoristenostNamirnicaRepository());
+
+                //Act
+                var uspjeh = service.AddIskoristenostNamirnice(iskoristenost);
+
+                //Assert
+                Assert.True(uspjeh);
+
+                transaction.Dispose();
+            }
+        }
+
+        [Fact]
+        public void UpdateIskoristenostNamirnice_GivenIskoristenostNamirniceIsNotNull_IskoristenostNamirniceIsUpdated()
+        {
+            //Arrange
+            using (var transaction = new TransactionScope())
+            {
+                IskoristenostNamirnicaServices service = new IskoristenostNamirnicaServices(new IskoristenostNamirnicaRepository());
+                var iskoristenost = new iskoristenost_namirnice()
+                {
+                    iskoristeno = 99,
+                    namirnica_u_katalogu_id = 552182865,
+                    datum = DateTime.Now
+                };
+
+                //Act
+                var uspjeh = service.UpdateIskoristenostNamirnice(iskoristenost);
+
+                //Assert
+                Assert.True(uspjeh);
+
+                transaction.Dispose();
+            }
+        }
+
+        [Fact]
+        public void GetIskoristeneNamirniceByMonth_GivenValidMonthAndYear_ReturnsList()
+        {
+            //Arrange
+            IskoristenostNamirnicaServices service = new IskoristenostNamirnicaServices(new IskoristenostNamirnicaRepository());
+
+            //Act
+            var iskoristeneNamirnice = service.GetIskoristeneNamirniceByMonth(2, 2023);
+
+            //Assert
+            Assert.IsType<List<StavkaIskoristenostNamirnice>>(iskoristeneNamirnice);
+        }
+    }
+}
