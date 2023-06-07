c6aa215685e5a14989853f47cc7ac27c3703a4ae
MatejRitosa <ritossamatej@gmail.com>
2023-06-07 19:18:05 +0200

    Popravljene greške u testovima ZaposlenikService.

--- Software/E-ugostiteljstvo/BusinessLogicLayer_IntegrationTests/ZaposlenikServices_IntegrationTests.cs
+++ Software/E-ugostiteljstvo/BusinessLogicLayer_IntegrationTests/ZaposlenikServices_IntegrationTests.cs
@@ -9,18 +9,16 @@ using System.Threading.Tasks;
 using System.Transactions;
 using Xunit;
 
-namespace BusinessLogicLayer_IntegrationTests {
-    public class ZaposlenikServices_IntegrationTests {
+
+   
 namespace BusinessLogicLayer_IntegrationTests
 {
     public class ZaposlenikServices_IntegrationTests
     {
         [Fact]
         public void AddZaposlenik_GivenZaposelnikIsNotNull_ZaposlenikIsCreated() {
-        public void GetZaposelnikById_GivenZaposlenikIdExists_ReturnsZaposlenik()
-        {
             // Arrange
-            using(var transaction = new TransactionScope()) {
+            using (var transaction = new TransactionScope()) {
                 ZaposlenikServices zaposlenikServices = new ZaposlenikServices(new ZaposlenikRepository());
                 byte[] byteArray = { 0x41, 0x42, 0x43, 0x44, 0x45 };
                 uloga novaUloga = new uloga {
@@ -28,8 +26,8 @@ namespace BusinessLogicLayer_IntegrationTests
                     naziv = "Kuhinja",
                     opis = null
                 };
-                zaposlenik zaposlenik = new zaposlenik { 
-                    
+                zaposlenik zaposlenik = new zaposlenik {
+
                     ime = "Pero",
                     prezime = "Peric",
                     email = "pperic@mail.com",
@@ -37,34 +35,40 @@ namespace BusinessLogicLayer_IntegrationTests
                     lozinka = "123456",
                     slika = byteArray
 
-            };
+                };
                 // Act
                 var uspjeh = zaposlenikServices.AddZaposlenik(zaposlenik);
                 // Assert
                 Assert.True(uspjeh);
 
             }
-            
+        }
+
+
+        public void GetZaposelnikById_GivenZaposlenikIdExists_ReturnsZaposlenik()
+        {
+            // Arrange
+            ZaposlenikServices zaposlenikServices = new ZaposlenikServices(new ZaposlenikRepository());
+            var id = 7;
+            //Act
+            var dohvaceniZaposelnik = zaposlenikServices.GetZaposlenikById(id);
+            Assert.Equal(dohvaceniZaposelnik.id, id);
         }
         [Fact]
         public void GetZaposelnikByEmail_GivenZaposlenikEmailExists_ReturnsZaposelnik() {
             // Arrange
             ZaposlenikServices zaposlenikServices = new ZaposlenikServices(new ZaposlenikRepository());
             string email = "test@mail.com";
-            var id = 7;
             //Act
             var dohvaceniZaposelnik = zaposlenikServices.GetZaposlenikByEmail(email);
-            var dohvaceniZaposelnik = zaposlenikServices.GetZaposlenikById(id);
+            
             //Assert
             Assert.Equal(dohvaceniZaposelnik.email, email);
-            Assert.Equal(dohvaceniZaposelnik.id, id);
+            
         }
 
         [Fact]
         public void GetZaposlenikNarudzbenice_GivenNarudzbenicaZaposelnikIdIsValid_ReturnsZaposlenik() {
-            //Arrange
-        public void GetZaposelnikById_GivenZaposlenikIdNotExists_ReturnsNull()
-        {
             // Arrange
             ZaposlenikServices zaposlenikServices = new ZaposlenikServices(new ZaposlenikRepository());
             int zaposlenikId = 5;
@@ -73,18 +77,28 @@ namespace BusinessLogicLayer_IntegrationTests
             //Assert
             Assert.Equal(dohvaceniZaposelnik.id, zaposlenikId);
         }
+            //Arrange
+        public void GetZaposelnikById_GivenZaposlenikIdNotExists_ReturnsNull()
+        {
+            //Arrange
+            ZaposlenikServices zaposlenikServices = new ZaposlenikServices(new ZaposlenikRepository());
+            var id = 1;
+            //Act
+            var dohvaceniZaposelnik = zaposlenikServices.GetZaposlenikById(id);
+            //Assert
+            Assert.Null(dohvaceniZaposelnik);
+        }
 
         [Fact]
         public void GetZaposlenici_ReturnsZaposlenici() {
             //Arrange
             ZaposlenikServices servis = new ZaposlenikServices(new ZaposlenikRepository());
-            var id = 1;
             //Act
             var zaposlenici = servis.GetZaposlenici();
-            var dohvaceniZaposelnik = zaposlenikServices.GetZaposlenikById(id);
+            
             //Assert
             Assert.IsType<List<zaposlenik>>(zaposlenici);
-            Assert.Null(dohvaceniZaposelnik);
+            
         }
 
     }
