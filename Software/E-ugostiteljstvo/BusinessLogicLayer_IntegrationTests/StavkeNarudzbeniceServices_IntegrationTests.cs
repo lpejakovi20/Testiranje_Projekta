4953d1ff75cdc7223c5c4a36dd2b2aeacd2e214b
MatejRitosa <ritossamatej@gmail.com>
2023-06-07 19:05:13 +0200

    Kreirani svi integracijski testovi Matej.

---
+++ Software/E-ugostiteljstvo/BusinessLogicLayer_IntegrationTests/StavkeNarudzbeniceServices_IntegrationTests.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLogicLayer.Services;
+using DataAccessLayer.Repositories;
+using EntitiesLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using Xunit;
+
+namespace BusinessLogicLayer_IntegrationTests {
+    public class StavkeNarudzbeniceServices_IntegrationTests {
+        [Fact]
+        public void AddStavkeNarudzbenice_GivenStavkaNarudzbeniceIsNotNull_StavkeNarudzbeniceIsCreated() {
+            // Arrange
+            using (var transaction = new TransactionScope()) {
+                StavkeNarudzbeniceServices stavkeNarudzbeniceServices = new StavkeNarudzbeniceServices(new StavkeNarudzbeniceRepository());
+                namirnica_narudzbenica stavka = new namirnica_narudzbenica {
+                    namirnica_u_katalogu_id = 965498656,
+                    narudzbenica_id = 809508601,
+                    kolicina = 20
+                };
+
+                // Act
+                var uspjeh = stavkeNarudzbeniceServices.AddStavkeNarudzbenice(stavka);
+                // Assert
+                Assert.True(uspjeh);
+            }
+        }
+    }
+}
---
+++ Software/E-ugostiteljstvo/BusinessLogicLayer_IntegrationTests/UlogaServices_IntegrationTests.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLogicLayer.Services;
+using DataAccessLayer.Repositories;
+using EntitiesLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BusinessLogicLayer_IntegrationTests {
+    public class UlogaServices_IntegrationTests {
+        [Fact]
+        public void GetUloge_ReturnsUloge() {
+
+            //Arrange
+            UlogaServices servis = new UlogaServices(new UlogaRepository());
+            //Act
+            var uloge = servis.GetUloge();
+            //Assert
+            Assert.IsType<List<uloga>>(uloge);
+
+        }
+    }
+}
