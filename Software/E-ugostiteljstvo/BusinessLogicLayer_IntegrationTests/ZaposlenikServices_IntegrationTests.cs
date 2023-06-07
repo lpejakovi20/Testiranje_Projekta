1b265dc7eae68eb2c6ab333455e60e1ce7a7e066
Lovro Pejakovic <lpejakovi20@foi.hr>
2023-06-07 17:44:09 +0200

    Implementirani integracijski testovi za metodu GetZaposlenikById.

---
+++ Software/E-ugostiteljstvo/BusinessLogicLayer_IntegrationTests/ZaposlenikServices_IntegrationTests.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLogicLayer.Services;
+using DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BusinessLogicLayer_IntegrationTests
+{
+    public class ZaposlenikServices_IntegrationTests
+    {
+        [Fact]
+        public void GetZaposelnikById_GivenZaposlenikIdExists_ReturnsZaposlenik()
+        {
+            // Arrange
+            ZaposlenikServices zaposlenikServices = new ZaposlenikServices(new ZaposlenikRepository());
+            var id = 7;
+            //Act
+            var dohvaceniZaposelnik = zaposlenikServices.GetZaposlenikById(id);
+            //Assert
+            Assert.Equal(dohvaceniZaposelnik.id, id);
+        }
+
+        [Fact]
+        public void GetZaposelnikById_GivenZaposlenikIdNotExists_ReturnsNull()
+        {
+            // Arrange
+            ZaposlenikServices zaposlenikServices = new ZaposlenikServices(new ZaposlenikRepository());
+            var id = 1;
+            //Act
+            var dohvaceniZaposelnik = zaposlenikServices.GetZaposlenikById(id);
+            //Assert
+            Assert.Null(dohvaceniZaposelnik);
+        }
+    }
+}
