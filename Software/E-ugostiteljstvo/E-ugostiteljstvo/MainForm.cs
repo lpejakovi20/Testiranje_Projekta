be7a504c44c988649d5ea7b16c4b946ff59e22a2
MatejRitosa <ritossamatej@gmail.com>
2023-06-10 17:58:02 +0200

    Popravljene hardkodirane putanje.

--- Software/E-ugostiteljstvo/E-ugostiteljstvo/MainForm.cs
+++ Software/E-ugostiteljstvo/E-ugostiteljstvo/MainForm.cs
@@ -13,6 +13,7 @@ using AForge.Video.DirectShow;
 using BusinessLogicLayer.Services;
 using DataAccessLayer.Repositories;
 using EntitiesLayer;
+using EntitiesLayer.Entities;
 using FaceRecognitionDotNet;
 
 namespace E_ugostiteljstvo
@@ -25,15 +26,20 @@ namespace E_ugostiteljstvo
         byte[] imageBytes;
         FilterInfoCollection filterInfoCollection;
         VideoCaptureDevice captureDevice;
+        private const string BaseDirectoryName = "source\\repos\\tkipp-nparag20-mritosa20-lpejakovi20\\Software\\E-ugostiteljstvo\\FaceRec";
+        private string baseDirectory;
         int departmentCheck;
 
         public MainForm()
         {
             InitializeComponent();
             txtLozinka.PasswordChar = '*';
+            baseDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), BaseDirectoryName);
             this.HelpRequested += MainForm_HelpRequested;
 
-
+            if (!Directory.Exists(baseDirectory)) {
+                Directory.CreateDirectory(baseDirectory);
+            }
         }
 
 
@@ -56,18 +62,12 @@ namespace E_ugostiteljstvo
                 LogiraniZaposlenik.Id = zaposlenik.id;
                 if (zaposlenik.uloga_id == 2)
                 {
-                    var frmKuhinja = new FrmKatalogNamirnica();
-                    Hide();
-                    frmKuhinja.ShowDialog();
-                    Close();
+                    PrebaciNaKuhinju();
 
                 }
                 else if (zaposlenik.uloga_id == 1)
                 {
-                    var frmRacunovodstvo = new FrmIzbornikRacunovodstvo();
-                    Hide();
-                    frmRacunovodstvo.ShowDialog();
-                    Close();
+                    PrebaciNaRacunovodstvo();
 
                 }
 
@@ -123,12 +123,11 @@ namespace E_ugostiteljstvo
 
 
             for (int i = 0; i < zaposlenici.Count; i++) {
+
+
                 string fileName = $"{i}.jpg";
-                string filePath = Path.Combine("C:/Users/38599/source/repos/rpp22-mritosa20-nparag20-lpejakovi20/Software/E-ugostiteljstvo/Users", fileName);
-
-
-
-
+                string filePath = Path.Combine(baseDirectory, "Users\\"+fileName);
+                
                 File.WriteAllBytes(filePath, zaposlenici[i].slika);
 
                 File.WriteAllText(Path.ChangeExtension(filePath, ".txt"), zaposlenici[i].uloga_id.ToString());
@@ -138,10 +137,10 @@ namespace E_ugostiteljstvo
 
 
             string fileName2 = "login.jpg";
-            string directoryPath2 = @"C:\Users\38599\source\repos\rpp22-mritosa20-nparag20-lpejakovi20\Software\E-ugostiteljstvo\LoginUsers";
+            string directoryPath2 = Path.Combine(baseDirectory, "LoginUsers");
             string filePath2 = Path.Combine(directoryPath2, fileName2);
             File.WriteAllBytes(filePath2, imageBytes);
-            string imagePath = @"C:\Users\38599\source\repos\rpp22-mritosa20-nparag20-lpejakovi20\Software\E-ugostiteljstvo\LoginUsers\login.jpg";
+            string imagePath = filePath2;
             IEnumerable<FaceEncoding> faceEncodings;
             using (Bitmap bitmap = new Bitmap(imagePath)) {
                 var knownImage = FaceRecognition.LoadImage(bitmap);
@@ -150,56 +149,13 @@ namespace E_ugostiteljstvo
 
 
             if (faceEncodings.Any()) {
-                var knownEncoding = faceEncodings.First();
-
-                var imagePaths = new List<string>();
-                for (int i = 0; i < zaposlenici.Count() - 1; i++) {
-                    imagePaths.Add("C:/Users/38599/source/repos/rpp22-mritosa20-nparag20-lpejakovi20/Software/E-ugostiteljstvo/Users/" + i + ".jpg");
-                };
-                bool checkFace = false;
-
-                for (int i = 1; i < zaposlenici.Count; i++) {
-
-                    var path = "C:/Users/38599/source/repos/rpp22-mritosa20-nparag20-lpejakovi20/Software/E-ugostiteljstvo/Users/" + i + ".jpg";
-                    var pathTxt = "C:/Users/38599/source/repos/rpp22-mritosa20-nparag20-lpejakovi20/Software/E-ugostiteljstvo/Users/" + i + ".txt";
-                    using (Bitmap bitmapCompare = new Bitmap(path)) {
-                        var imageToCompare = FaceRecognition.LoadImage(bitmapCompare);
-                        var encodingsToCompare = faceRecognition.FaceEncodings(imageToCompare);
-                        string department = File.ReadAllText(pathTxt);
-
-                        if (bitmapCompare != null) {
-
-
-                            foreach (var encodingToCompare in encodingsToCompare) {
-                                var compare = FaceRecognition.CompareFace(knownEncoding, encodingToCompare);
-                                if (compare) {
-                                    if (department == "1") {
-                                        departmentCheck = 1;
-                                    } else {
-                                        departmentCheck = 2;
-                                    }
-                                    checkFace = true;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    if (checkFace == true) {
-                        break;
-                    }
-                }
+                bool checkFace = PrepoznajLice(faceRecognition, zaposlenici, faceEncodings);
                 if (checkFace) {
                     if (departmentCheck == 2) {
-                        var frmKuhinja = new FrmKatalogNamirnica();
-                        Hide();
-                        frmKuhinja.ShowDialog();
-                        Close();
+                        PrebaciNaKuhinju();
 
                     } else {
-                        var frmRacunovodstvo = new FrmIzbornikRacunovodstvo();
-                        Hide();
-                        frmRacunovodstvo.ShowDialog();
-                        Close();
+                        PrebaciNaRacunovodstvo();
 
                     }
 
@@ -211,6 +167,65 @@ namespace E_ugostiteljstvo
             } else {
                 MessageBox.Show("No Face Detected");
             }
+        }
+
+        private void PrebaciNaRacunovodstvo() {
+            var frmRacunovodstvo = new FrmIzbornikRacunovodstvo();
+            Hide();
+            frmRacunovodstvo.ShowDialog();
+            Close();
+        }
+
+        private void PrebaciNaKuhinju() {
+            var frmKuhinja = new FrmKatalogNamirnica();
+            Hide();
+            frmKuhinja.ShowDialog();
+            Close();
+        }
+
+        private bool PrepoznajLice(FaceRecognition faceRecognition, List<zaposlenik> zaposlenici, IEnumerable<FaceEncoding> faceEncodings) {
+            var knownEncoding = faceEncodings.First();
+            var imageDirectory = Path.Combine(baseDirectory, "Users");
+
+            var imagePaths = new List<string>();
+            for (int i = 0; i < zaposlenici.Count() - 1; i++) {
+                imagePaths.Add(Path.Combine(imageDirectory, i + ".jpg"));
+            };
+            bool checkFace = false;
+
+            for (int i = 1; i < zaposlenici.Count; i++) {
+
+                var path = Path.Combine(imageDirectory, i + ".jpg");
+
+                var pathTxt = Path.Combine(imageDirectory, i + ".txt");
+                using (Bitmap bitmapCompare = new Bitmap(path)) {
+                    var imageToCompare = FaceRecognition.LoadImage(bitmapCompare);
+                    var encodingsToCompare = faceRecognition.FaceEncodings(imageToCompare);
+                    string department = File.ReadAllText(pathTxt);
+
+                    if (bitmapCompare != null) {
+
+
+                        foreach (var encodingToCompare in encodingsToCompare) {
+                            var compare = FaceRecognition.CompareFace(knownEncoding, encodingToCompare);
+                            if (compare) {
+                                if (department == "1") {
+                                    departmentCheck = 1;
+                                } else {
+                                    departmentCheck = 2;
+                                }
+                                checkFace = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (checkFace == true) {
+                    break;
+                }
+            }
+
+            return checkFace;
         }
 
         private void PretvoriUByteArray() {
